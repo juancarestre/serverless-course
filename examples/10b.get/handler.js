30fa858f8ead345a29ede501ddc6524945803e50
juan restrepo <jcrestrepobedoya@gmail.com>
2022-07-28 13:29:05 -0500

    harper lewis

--- examples/10b.get/handler.js
+++ examples/10b.get/handler.js
@@ -1,12 +1,17 @@
 const AWS = require("aws-sdk")
-const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    accessKeyId: 'YOUR-ACCESSKEYID' ,
+    secretAccessKey: 'YOUR-SECRETACCESSKEY' ,
+    region: "local",
+    endpoint: 'http://localhost:8000',
+  })
 
 const getUser = async (event, context) => {
     const params = {
         TableName: 'user-crud-sls-app',
-        KeyConditionExpression: '#pk = :pk',
-        ExpressionAttributeValues: { ':pk': '1' },
-        ExpressionAttributeNames: { '#pk': 'pk' }
+        KeyConditionExpression: 'pk = :pk',
+        ExpressionAttributeValues: { ':pk': '1' }
     }
     const user = await dynamoDB.query(params).promise()
     return {
